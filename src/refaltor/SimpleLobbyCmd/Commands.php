236b918887e9c48668545c054637672d2ba2f8da
Refaltor <77900181+Refaltor77@users.noreply.github.com>
2021-07-11 23:36:57 +0200

    Add files via upload

---
+++ src/refaltor/SimpleLobbyCmd/Commands.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace refaltor\SimpleLobbyCmd;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\level\Position;
+use pocketmine\Player;
+use pocketmine\plugin\Plugin;
+use pocketmine\Server;
+
+class Commands extends Command
+{
+    public int $x;
+    public int $y;
+    public int $z;
+    public string $level;
+    public Plugin $plugin;
+    public ?string $message;
+
+    public function __construct(int $x, int $y, int $z, string $level, Plugin $plugin, string $name, string $description = "", ?string $message = null, string $usageMessage = null, array $aliases = [])
+    {
+        $this->x = $x;
+        $this->y = $y;
+        $this->z = $z;
+        $this->level = $level;
+        $this->plugin = $plugin;
+        $this->message = $message;
+
+        parent::__construct($name, $description, $usageMessage, $aliases);
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args)
+    {
+        if (!$sender instanceof Player) return;
+        $level = Server::getInstance()->getLevelByName($this->level);
+        if (is_null($level)){
+            $this->plugin->getServer()->getLogger()->critical("§c[§4WARNING§c] §cLevel '{$this->level}' is null");
+            $this->plugin->getServer()->getPluginManager()->disablePlugin($this->plugin);
+        }
+        $position = new Position($this->x, $this->y, $this->z, $level);
+        $sender->teleport($position);
+        if (!is_null($this->message)) $sender->sendMessage($this->message);
+    }
+}
---
+++ src/refaltor/SimpleLobbyCmd/Main.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace refaltor\SimpleLobbyCmd;
+
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase
+{
+    public function onEnable()
+    {
+        $this->saveResource('config.yml');
+        $array = $this->getConfig()->get('commands');
+        $command = $this->getServer()->getCommandMap();
+        foreach ($array as $name => $keys){
+            $command->register($name, new Commands($keys['x'], $keys['y'], $keys['z'], $keys['world'], $this, $name, $keys['description'], $keys['message']));
+            $this->getServer()->getLogger()->info('§c'.$name.' loaded !');
+        }
+    }
+}
